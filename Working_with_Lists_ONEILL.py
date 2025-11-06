bd9f75da6752e0eafbfe5d739ac3f7fe58d3de3e
Ian O'Neill <ioneill@mail.walshcollege.edu>
2025-11-05 22:47:25 -0500

    listed the courses

--- Working_with_Lists_ONEILL.py
+++ Working_with_Lists_ONEILL.py
@@ -2,3 +2,11 @@
 # IT 410
 # Week 6 - Working with Lists Assignment
 # Professor Charnesky
+
+# List of all the courses I have taken at Walsh
+courses_taken = ["com 405", "it 405", "it 407", "it 410"]
+
+# Sort the list and print each course in uppercase
+courses_taken.sort()
+for course in courses_taken:
+    print("I have taken " + course.upper() + " at Walsh College.")
