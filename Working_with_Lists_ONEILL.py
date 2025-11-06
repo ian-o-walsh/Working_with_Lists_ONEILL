cba20d85444ea6b51492d04f01be72145c62974a
Ian O'Neill <ioneill@mail.walshcollege.edu>
2025-11-05 22:47:55 -0500

    Making progress on assignment

--- Working_with_Lists_ONEILL.py
+++ Working_with_Lists_ONEILL.py
@@ -10,3 +10,24 @@ courses_taken = ["com 405", "it 405", "it 407", "it 410"]
 courses_taken.sort()
 for course in courses_taken:
     print("I have taken " + course.upper() + " at Walsh College.")
+
+# Add my courses planned for next term, resort, and print all
+courses_taken.extend(["it 412", "it 461"])
+courses_taken.sort()
+
+print("\nThis is my course of study with upcoming courses added:")
+for course in courses_taken:
+    print(course.upper())
+
+# Remove the courses I've already taken and print them
+completed_courses = ["com 405", "it 405", "it 407", "it 410"]
+
+print("\nI do not have to take these courses:")
+for course in completed_courses:
+    print(course.upper())
+    courses_taken.remove(course)
+
+# Print my courses left to take
+print("\nI plan to take the following courses next term:")
+for course in courses_taken:
+    print(course.upper())
